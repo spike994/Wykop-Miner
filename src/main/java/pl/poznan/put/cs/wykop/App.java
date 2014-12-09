a96ca419677a34fda8546184ad24813350a98142
Dawid Kłos <dawid.b.klos@gmail.com>
2014-12-09 06:30:59 +0100

    Handlem more fluent app Controll

--- src/main/java/pl/poznan/put/cs/wykop/App.java
+++ src/main/java/pl/poznan/put/cs/wykop/App.java
@@ -1,5 +1,6 @@
 
         package pl.poznan.put.cs.wykop;
+        import org.apache.commons.io.FileUtils;
         import org.hibernate.Criteria;
         import org.hibernate.Session;
         import org.hibernate.Transaction;
@@ -7,14 +8,15 @@
         import org.hibernate.exception.ConstraintViolationException;
         import pl.poznan.put.cs.wykop.api.Api;
         import pl.poznan.put.cs.wykop.connection.ConnectionException;
+        import pl.poznan.put.cs.wykop.connection.WykopException;
         import pl.poznan.put.cs.wykop.json.JsonException;
         import pl.poznan.put.cs.wykop.model.Entry;
         import pl.poznan.put.cs.wykop.service.ConfigManager;
         import pl.poznan.put.cs.wykop.service.EntryService;
         import pl.poznan.put.cs.wykop.service.SessionManager;
 
+        import java.io.File;
         import java.io.IOException;
-        import java.io.PrintWriter;
 
         public class App {
     public static void main(String[] args) throws IOException, ConnectionException, JsonException {
@@ -31,25 +33,24 @@
                 .createCriteria(Entry.class)
                 .setProjection(Projections.min("id"));
         long lastEntry = (Long) criteria.uniqueResult();
-        PrintWriter writer = new PrintWriter("ConnectionExceptionLog.txt", "UTF-8");
 
-        for (int i =(int) lastEntry-1; i > lastEntry-500; i--) {
+        for (int i =(int) lastEntry-1; i > 0; i--) {
             transaction = session.beginTransaction();
             try {
                 Entry entry = api.getEntryString(i);
                 EntryService.save(entry, session);
                 transaction.commit();
                 System.out.println(entry.getId());
-            }catch (ConnectionException e) {
-                writer.println(i);
+            } catch (WykopException e) {
                 transaction.rollback();
-            }catch (ConstraintViolationException e){
+            } catch (ConnectionException e) {
+                FileUtils.writeStringToFile(new File("ConnectionExceptionLog.txt"), i+"\n", true);
+                transaction.rollback();
+            } catch (ConstraintViolationException e) {
                 System.out.println("Constraint Violation Exception");
                 transaction.rollback();
             }
         }
-
-        writer.close();
         session.close();
     }
 }
