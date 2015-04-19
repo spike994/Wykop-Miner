573fddd7154ebd644bba97bee4d59d7d2fa2b228
spike994 <dawid.klos@student.put.edu.pl>
2015-04-19 12:31:38 +0200

    Profile download with User  model refactor

--- src/main/java/pl/poznan/put/cs/wykop/App.java
+++ src/main/java/pl/poznan/put/cs/wykop/App.java
@@ -58,31 +58,6 @@ public class App {
             }
         }
 
-
-//        for (int i = (int) first; i<(int) last ; i++) {
-//            transaction = session.beginTransaction();
-//
-//            try {
-//                String response = api.getLinksCommentsString(i);
-//                LinkComment[] comments = mapper.readValue(response, LinkComment[].class);
-//                for(LinkComment linkComment : comments){
-//                    linkComment.setLinkId(i);
-//                    LinkCommentService.save(linkComment,session);
-//                    System.out.println(linkComment.getId()+"  "+linkComment.getDate());
-//                }
-//                transaction.commit();
-//                FileUtils.writeStringToFile(new File("saved.txt"), i+"\n",true);
-//
-//            } catch (WykopException e) {
-//                transaction.rollback();
-//            } catch (ConnectionException e) {
-//                FileUtils.writeStringToFile(new File("LinkConnExcLog.txt"), i + "\n", true);
-//                transaction.rollback();
-//            } catch (ConstraintViolationException e) {
-//                System.out.println("Constraint Violation Exception");
-//                transaction.rollback();
-//            }
-//        }
         session.close();
     }
 
